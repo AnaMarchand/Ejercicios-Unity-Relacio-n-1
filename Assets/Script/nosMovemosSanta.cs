8a999ecc52b7e53dbd1db72734ea1fa7e2c5ee11
AnaMarchand <diseno@iMac-de-Diseno-232.local>
2018-11-19 14:01:19 +0100

    Ejercicio Unity
    Paso 1

---
+++ Assets/Script/nosMovemosSanta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nosMovemosSanta : MonoBehaviour {
+    Rigidbody2D Santa;
+    public float velocidadMaxima = 5;
+
+	// Use this for initialization
+	void Start () {
+        
+        Santa = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        Santa.velocity = new Vector2(h, 0) * velocidadMaxima;
+	}
+}
